e9d0d4d48449ae4bdce29814dfa75b4af92d5665
Lauraluu04 <lml9874@mavs.uta.edu>
2026-04-19 22:25:45 -0500

    Update allowedOrigin.js
    Update allowedOrigin.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    'https://caloriecount.onrender.com'
+    'https://caloriecount.onrender.com',
+    'https://calorieapp-7sml.onrender.com'
 ];
 
 module.exports = allowedOrigins;
